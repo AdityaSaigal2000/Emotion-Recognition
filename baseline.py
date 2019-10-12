8e73ef07d202189aceed7b82e3b8b7a56c39d654
ChristopherAlexiev <chris340a@gmail.com>
2019-10-12 15:12:13 -0400

    Update baseline.py
    added website reference and slightly changed format. note this file has the ideas not in their final form

--- baseline.py
+++ baseline.py
@@ -1,20 +1,38 @@
 
-
+#####THE FACE RECOGNITION###################################
+#source: http://gregblogs.com/computer-vision-cropping-faces-from-images-using-opencv2/
 # Importing the libraries
 import numpy as np
 import cv2
 
+import cv2
+
+def facechop(image):  
+    facedata = "haarcascade_frontalface_default.xml"
+    cascade = cv2.CascadeClassifier(facedata)
+
+    img = cv2.imread(image)
+
+    minisize = (img.shape[1],img.shape[0])
+    miniframe = cv2.resize(img, minisize)
+
+    faces = cascade.detectMultiScale(miniframe)
+
+    for f in faces:
+        x, y, w, h = [ v for v in f ]
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255))
+
+        sub_face = img[y:y+h, x:x+w]
+        face_file_name = "faces/face_" + str(y) + ".jpg"
+        cv2.imwrite(face_file_name, sub_face) #save each face image
+
+    cv2.imshow(image, img)
+
+    return
+
+
+####THE DECISION TREE###################
 # Importing the dataset
-
-""" face detection using openCV """
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Detect faces
-faces = face_cascade.detectMultiScale(face_cascade, 1.1, 4)
-# Draw rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
 
 """ Decision Trees """
 # Splitting the dataset into the Training set and Test set
@@ -39,29 +57,4 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-'''
-import cv2
 
-def facechop(image):  
-    facedata = "haarcascade_frontalface_default.xml"
-    cascade = cv2.CascadeClassifier(facedata)
-
-    img = cv2.imread(image)
-
-    minisize = (img.shape[1],img.shape[0])
-    miniframe = cv2.resize(img, minisize)
-
-    faces = cascade.detectMultiScale(miniframe)
-
-    for f in faces:
-        x, y, w, h = [ v for v in f ]
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255))
-
-        sub_face = img[y:y+h, x:x+w]
-        face_file_name = "faces/face_" + str(y) + ".jpg"
-        cv2.imwrite(face_file_name, sub_face)
-
-    cv2.imshow(image, img)
-
-    return
-'''
